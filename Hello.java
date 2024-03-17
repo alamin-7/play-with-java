f511efe6b0cf928584fc57f16e9398bd2ef1e51d
alamin-7 <alamin188914@gmail.com>
2024-03-18 03:53:32 +0600

    inheritance explored

--- Hello.java
+++ Hello.java
@@ -1,7 +1,100 @@
+class Employye{
+    int bonus = 1000;
+
+    String getNotification(){
+        return "Holiday!";
+    }
+
+    void add(){
+        System.out.println("Employye");
+    }
+}
+
+class Engineer extends Employye{
+    int salary;
+    int id;
+    String name;
+
+    public Engineer(){
+        System.out.println("Engineer constructor");
+        add(); // overriden method will be called. class c add() will be called
+        super.add();
+    }
+
+    void add(){
+        System.out.println("Engineer");
+    }
+
+    void setID(int id){
+        this.id = id;
+    }
+    void setSalary(int salary){
+        this.salary = salary;
+    }
+    void setName(String name){
+        this.name = name;
+    }
+    void getEngg(){
+        System.out.println("From class Engineer");
+    }
+}
+
+class Software extends Engineer{
+    int salary;
+    int id;
+    String name;
+
+    // public Software(){
+        //super.add();
+    //}
+
+    void add(){
+        System.out.println("Software");
+    }
+
+    void getTest(){
+        System.out.println("From Software");
+    }
+}
+
+class Manager extends Employye{
+    int id;
+    String name;
+    int salary;
+
+    void setID(int id){
+        this.id = id;
+    }
+    void setSalary(int salary){
+        this.salary = salary;
+    }
+    void setName(String name){
+        this.name = name;
+    }
+}
+
+
 class Hello{
 
     public static void main(String args[]){
 
-        System.out.println("Hello world");
+       /*  Engineer engineer1 = new Engineer();
+        engineer1.setID(1);
+        engineer1.setName("Alamin");
+        engineer1.setSalary(1000);
+
+        Manager manager1 = new Manager();
+
+        manager1.setID(1);
+        manager1.setName("Russel");
+        manager1.setSalary(10000);*/
+
+        //System.out.println(engineer1.getNotification());
+
+        Software software1 = new Software();
+
+        //software1.add();
+
+
     }
 }
