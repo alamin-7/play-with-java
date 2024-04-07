291199d20ae676ec753a764c52e2d11ba09c3e20
alamin-7 <alamin188914@gmail.com>
2024-04-08 01:11:33 +0600

    java string explored

---
+++ StringTest.java
@@ -0,0 +1,13 @@
+
+public class StringTest {
+    public static void main(String[] args) {
+        
+        String s = "Hello";
+        String s1 = "Hello";
+        String str = new String("Hello world");
+        str = "Hello";
+
+        System.out.println(str);
+        System.out.println(s1==s); // true
+    }
+}
