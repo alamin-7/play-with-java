698aae271f0fc60e9b3c7a0d09072f67e3c3cc41
alamin-7 <alamin188914@gmail.com>
2024-08-08 17:25:50 +0600

    Runnable interface added with lamda expression

--- MultiThreadTest.java
+++ MultiThreadTest.java
@@ -1,15 +1,19 @@
-class Person extends Thread{
+class Test extends Thread{
 
-     public void run(){
-        
+}
+
+/*class Person implements Runnable{
+
+    public void run(){
+
         for(int i = 1; i <= 5; i++){
             System.out.println("Person "+i);
             try{Thread.sleep(100);}catch(Exception e){}
         }
-     }
-}
+    }
+}*/
 
-class Employee extends Thread {
+/*class Employee implements Runnable {
 
     public void run(){
 
@@ -18,17 +22,49 @@ class Employee extends Thread {
             try{Thread.sleep(100);}catch(Exception e){}
         }
     }
-}
+}*/
 
 
-public class MultiThreadTest {
+public class Main {
     public static void main(String args []){
 
-        Person person1 = new Person();
-        Employee employee1 = new Employee();
+      /* Person person1 = new Person();
+       Employee employee1 = new Employee();*/
 
-        person1.start();
+    /*    Runnable obj1 = () ->
+           {
+                for(int i = 1; i <= 5; i++){
+                    System.out.println("Person "+i);
+                    try{Thread.sleep(100);}catch(Exception e){}
+                }
+           };*/
+
+  /*      Runnable obj2 = () ->
+            {
+
+                for(int i = 1; i <= 5; i++){
+                    System.out.println("Employee "+i);
+                    try{Thread.sleep(100);}catch(Exception e){}
+                }
+            };*/
+
+
+        Thread p1 = new Thread(() ->
+        {
+            for(int i = 1; i <= 5; i++){
+                System.out.println("Person "+i);
+                try{Thread.sleep(100);}catch(Exception e){}
+            }
+        });
+        Thread e1 = new Thread(() ->{
+            for(int i = 1; i <= 5; i++){
+                System.out.println("Employee "+i);
+                try{Thread.sleep(100);}catch(Exception e){}
+            }
+        });
+
+        p1.start();
         try{Thread.sleep(100);}catch(Exception e){}
-        employee1.start();
+        e1.start();
     }
 }
